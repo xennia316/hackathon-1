e7e3824636356e094a5a8ddac90f9a6521437584
Rha'ah <acharhaah2003@gmial.com>
2022-05-07 17:52:25 +0100

    initial comit

---
+++ Hackathon/script.js
@@ -0,0 +1,20 @@
+var hero = document.querySelector(".hero");
+var section2 = document.querySelector(".about");
+var section12 = document.querySelector(".about-text");
+function bgChanger(){
+    if(this.scrollY > hero.clientHeight/2){
+        hero.style.background = "white"
+    }
+    else{
+        hero.style.background = "#6f2dbd"
+    }
+    if(this.scrollY > (hero.clientHeight + section2.clientHeight + section12.clientHeight)){
+        console.log("yes");
+        document.body.style.background = "#2dc7ff";
+    }
+    else{
+        document.body.style.background = "#fff";
+    }
+    
+}
+window.addEventListener("scroll", bgChanger)
