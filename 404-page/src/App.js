4d700e8a9d322ec4c7e3b4fecea0bd00ed059b6d
xennia316 <xennia316@gmail.com>
2022-07-30 21:46:15 +0100

    created react app for 404 page

---
+++ 404-page/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return <BrowserRouter></BrowserRouter>;
+};
+
+export default App;
